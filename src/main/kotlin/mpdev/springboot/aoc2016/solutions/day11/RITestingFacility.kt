b0866ab0ad33f868ff32381e11680816c643bba6
marinos <marinosch.99981@gmail.com>
2024-04-22 10:09:00 +0200

    Day 11 part 1 optimised usig A* and reducing the neighbour states but still slow

--- src/main/kotlin/mpdev/springboot/aoc2016/solutions/day11/RITestingFacility.kt
+++ src/main/kotlin/mpdev/springboot/aoc2016/solutions/day11/RITestingFacility.kt
@@ -6,7 +6,7 @@ import mpdev.springboot.aoc2016.solutions.day11.Generator.*
 import mpdev.springboot.aoc2016.solutions.day11.MicroChip.*
 import mpdev.springboot.aoc2016.utils.AocException
 import mpdev.springboot.aoc2016.utils.SGraph
-import mpdev.springboot.aoc2016.utils.dijkstra
+import mpdev.springboot.aoc2016.utils.aStar
 import org.springframework.stereotype.Component
 import java.util.*
 import kotlin.math.abs
@@ -15,7 +15,8 @@ import kotlin.math.abs
 class RITestingFacility(inputDataReader: InputDataReader): PuzzleSolver(inputDataReader, 11) {
 
     var states = mutableListOf<RIState>()
-    val graph = SGraph<RIState>(getConnected = { state -> state.getNeighbourStates().map { Pair(it,1) }.toSet() })
+    val graph = SGraph<RIState>(getConnected = { state -> state.getNeighbourStates().map { Pair(it,1) }.toSet() },
+        heuristic = { state -> state.heuristic() })
 
     override fun initialize() {
         states.clear()
@@ -25,30 +26,6 @@ class RITestingFacility(inputDataReader: InputDataReader): PuzzleSolver(inputDat
                 T to 2, R to 2, r to 2, C to 2, c to 2,
                 t to 3
             )))
-        /*states.add(RIState(
-            mutableMapOf(
-                S to 1, s to 1,
-                R to 2, r to 2, C to 2, c to 2,
-                E to 3, T to 3, t to 3,  P to 3, p to 3,
-            )))
-        states.add(RIState(
-            mutableMapOf(
-                S to 1, s to 1,
-                R to 2, r to 2, C to 2, c to 2,
-                E to 4, T to 4, t to 4,  P to 4, p to 4,
-            )))
-        states.add(RIState(
-            mutableMapOf(
-                S to 1, s to 1,
-                R to 2, r to 2, C to 2, c to 2,
-                E to 4, T to 4, t to 4, P to 4, p to 4, S to 4, s to 4,
-            )))
-        states.add(RIState(
-            mutableMapOf(
-                S to 1, s to 1,
-                E to 3, R to 3, r to 3, C to 3, c to 3,
-                T to 4, t to 4, P to 4, p to 4, S to 4, s to 4,
-            )))*/
         states.add(RIState((generators + microchips + E).associateWith { 4 }.toMutableMap()))
     }
 
@@ -56,9 +33,10 @@ class RITestingFacility(inputDataReader: InputDataReader): PuzzleSolver(inputDat
     override fun solvePart1(): Int {
         var moves = 0
         var iterations = 0
+        previousStates.clear()
         log.info("start state\n{}", states[0])
         for (i in 0..<states.lastIndex) {
-            val minPath = graph.dijkstra(states[i]) { state -> state == states[i + 1] }
+            val minPath = graph.aStar(states[i]) { state -> state == states[i + 1] }
             moves += minPath.minCost
             iterations += minPath.numberOfIterations
             log.info("{} moves to state\n{}", moves, states[i+1])
@@ -76,9 +54,23 @@ class RITestingFacility(inputDataReader: InputDataReader): PuzzleSolver(inputDat
     }
 }
 
+val previousStates = mutableSetOf<RIState>()
+
 data class RIState(val state: MutableMap<Any,Int>) {
 
-    fun getNeighbourStates() = getNeighbours().map { getNewState(it.first, it.second, it.third) }
+    fun heuristic() = state.values.sumOf { 4 - it }
+
+    fun getNeighbourStates(): List<RIState> {
+        val newStates = mutableListOf<RIState>()
+        getNeighbours().forEach {
+            val newState = getNewState(it.first, it.second, it.third)
+            if (!previousStates.contains(newState)) {
+                previousStates.add(newState)
+                newStates.add(newState)
+            }
+        }
+        return newStates
+    }
 
     fun getNewState(elements: Set<Any>, from: Int, to: Int): RIState {
         val newState = RIState(this.state.toMutableMap())
@@ -95,36 +87,85 @@ data class RIState(val state: MutableMap<Any,Int>) {
         val currentLevel = state[RITestingFacility.E] ?: throw AocException("Elevator not found...")
         val generatorsThisLevel = state.filter { it.value == currentLevel && it.key is Generator }.map { it.key as Generator }.toSet()
         val microChipsThisLevel = state.filter { it.value == currentLevel && it.key is MicroChip }.map { it.key as MicroChip }.toSet()
-        val destinations = when (currentLevel) {
-            1 -> setOf(2)
-            4 -> setOf(3)
-            else -> setOf(currentLevel + 1, currentLevel - 1)
-        }
         val neighbours: MutableSet<Triple<Set<Any>, Int, Int>> = mutableSetOf()
-        // generators
-        destinations.forEach { to ->
-            generatorsThisLevel.forEach { g1 ->
-                generatorsThisLevel.forEach { g2 ->
-                    if (canMove(setOf(g1, g2), currentLevel, to))
-                        neighbours.add(Triple(setOf(RITestingFacility.E, g1, g2), currentLevel, to))
-        } } }
-        // chips
-        destinations.forEach { to ->
-            val microChipsThatCanMove = microChipsThisLevel.filter { m -> canMove(m, to) }.toSet()
-            microChipsThatCanMove.forEach { m -> neighbours.add(Triple(setOf(RITestingFacility.E, m), currentLevel, to)) }
-            microChipsThatCanMove.forEach { m1 ->
-                microChipsThatCanMove.forEach { m2 ->
-                    if (m1 != m2) neighbours.add(Triple(setOf(RITestingFacility.E, m1, m2), currentLevel, to))
-                }
-            }
+        val upLevel = if (currentLevel == 4) -1 else currentLevel + 1
+        val downLevel = if (currentLevel == 1) -1 else currentLevel - 1
+        // see what can move up
+        if (upLevel > 0) {
+            getMoveUpSet(currentLevel, upLevel, generatorsThisLevel, microChipsThisLevel, neighbours)
         }
-        // generator + chip
-        destinations.forEach { to -> generatorsThisLevel.forEach { g ->
-            if (microChipsThisLevel.contains(g.toMicrochip()))
-                neighbours.add(Triple(setOf(RITestingFacility.E, g, g.toMicrochip()), currentLevel, to))
-        } }
-        pruneStates(neighbours)
+        // see what can move down
+        if (downLevel > 0) {
+            getMoveDownSet(currentLevel, downLevel, generatorsThisLevel, microChipsThisLevel, neighbours)
+        }
         return neighbours
+    }
+
+    private fun getMoveUpSet(currentLevel: Int, upLevel: Int, generatorsThisLevel: Set<Generator>, microChipsThisLevel: Set<MicroChip>,
+                             neighbours: MutableSet<Triple<Set<Any>, Int, Int>>) {
+        // generators
+        var generatorsUp: Set<Generator> = setOf()
+        outerLoop@ for (g1 in generatorsThisLevel)
+            for (g2 in (generatorsThisLevel - g1))
+                if (canMove(setOf(g1, g2), currentLevel, upLevel)) {
+                    generatorsUp = setOf(g1, g2)
+                    break@outerLoop
+                }
+        // if 2 generators can move up we don't move just one, else we find one that can move up
+        if (generatorsUp.isEmpty()) {
+            for (g in (generatorsThisLevel))
+                if (canMove(setOf(g), currentLevel, upLevel)) {
+                    generatorsUp = setOf(g)
+                    break
+                }
+        }
+        if (generatorsUp.isNotEmpty())
+            neighbours.add(Triple(generatorsUp + RITestingFacility.E, currentLevel, upLevel))
+
+        // microchips
+        val microChipsUp = microChipsThisLevel.filter { m -> canMove(m, upLevel) }
+        // if two can move up then we move two else just one
+        if (microChipsUp.size >= 2)
+            neighbours.add(Triple(setOf(RITestingFacility.E, microChipsUp[0], microChipsUp[1]), currentLevel, upLevel))
+        else if (microChipsUp.isNotEmpty())
+            neighbours.add(Triple(setOf(RITestingFacility.E, microChipsUp[0]), currentLevel, upLevel))
+
+        // generator + microchip pair
+        for (g in (generatorsThisLevel))
+            if (microChipsThisLevel.contains(g.toMicrochip()) && canMovePair(upLevel)) {
+                neighbours.add(Triple(setOf(RITestingFacility.E, g, g.toMicrochip()), currentLevel, upLevel))
+                break
+            }
+    }
+
+    private fun getMoveDownSet(currentLevel: Int, downLevel: Int, generatorsThisLevel: Set<Generator>, microChipsThisLevel: Set<MicroChip>,
+                             neighbours: MutableSet<Triple<Set<Any>, Int, Int>>) {
+        // generators
+        val generatorsDown = generatorsThisLevel.filter { g -> canMove(setOf(g), currentLevel, downLevel) }
+        // if 1 generator can move down we don't move two, else we look for a pair of then that can move down
+        if (generatorsDown.isNotEmpty())
+            neighbours.add(Triple(setOf(generatorsDown.first(), RITestingFacility.E), currentLevel, downLevel))
+        else {
+            outerLoop@ for (g1 in generatorsThisLevel)
+                for (g2 in (generatorsThisLevel - g1))
+                    if (canMove(setOf(g1, g2), currentLevel, downLevel)) {
+                        neighbours.add(Triple(setOf(g1, g2, RITestingFacility.E), currentLevel, downLevel))
+                        break@outerLoop
+                    }
+        }
+
+        // microchips
+        val microChipsDown = microChipsThisLevel.filter { m -> canMove(m, downLevel) }
+        // if one can move down then we move one
+        if (microChipsDown.isNotEmpty())
+            neighbours.add(Triple(setOf(RITestingFacility.E, microChipsDown.first()), currentLevel, downLevel))
+
+        // generator + microchip pair
+        for (g in (generatorsThisLevel))
+            if (microChipsThisLevel.contains(g.toMicrochip()) && canMovePair(downLevel)) {
+                neighbours.add(Triple(setOf(RITestingFacility.E, g, g.toMicrochip()), currentLevel, downLevel))
+                break
+            }
     }
 
     private fun canMove(generators: Set<Generator>, from: Int, to: Int): Boolean {
@@ -143,55 +184,15 @@ data class RIState(val state: MutableMap<Any,Int>) {
         return generatorsTo.isEmpty() || generatorsTo.any { g -> g == microChip.toGenerator() }
     }
 
+    private fun canMovePair(to: Int): Boolean {
+        val elementsTo = state.entries.filter { it.value == to }.map { it.key }
+        val generatorsTo = elementsTo.filterIsInstance<Generator>().toSet()
+        val microChipsTo = elementsTo.filterIsInstance<MicroChip>().toSet()
+        return microChipsTo.all { m -> isMicroChipSafe(m, generatorsTo) }
+    }
+
     private fun isMicroChipSafe(microChip: MicroChip, generators: Set<Generator>) =
         generators.isEmpty() || generators.contains(microChip.toGenerator())
-
-    private fun pruneStates(states: MutableSet<Triple<Set<Any>, Int, Int>>) {
-        val copyStates = states.toSet()
-        // if we can move 2 items up, we will not move any of them as single item up
-        copyStates.filter { it.third > it.second && it.first.size == 3 }.forEach { s ->
-            s.first.forEach { i -> states.remove(Triple(setOf(RITestingFacility.E, i), s.second, s.third)) }
-        }
-        // if we can move any 2 items down individually, we will not move both of them down as a pair
-        copyStates.filter { s -> s.third < s.second && s.first.size == 3 }.forEach { s ->
-            if ((s.first - RITestingFacility.E).all { i -> copyStates.contains(Triple(setOf(RITestingFacility.E, i), s.second, s.third)) })
-                states.remove(s)
-        }
-        // check for "equivalent states" and keep only one of them
-        copyStates.filter { s -> s.first.size == 2 && s.third > s.second && (s.first - RITestingFacility.E).all { it is MicroChip } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 2 && s.third < s.second && (s.first - RITestingFacility.E).all { it is MicroChip } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 2 && s.third > s.second && (s.first - RITestingFacility.E).all { it is Generator } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 2 && s.third < s.second && (s.first - RITestingFacility.E).all { it is Generator } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third > s.second && (s.first - RITestingFacility.E).all { it is MicroChip } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third < s.second && (s.first - RITestingFacility.E).all { it is MicroChip } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third > s.second && (s.first - RITestingFacility.E).all { it is Generator } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third < s.second && (s.first - RITestingFacility.E).all { it is Generator } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third > s.second &&
-                (s.first - RITestingFacility.E).any { it is Generator } &&  (s.first - RITestingFacility.E).any { it is MicroChip } }
-            .also { keepOne(it, states) }
-        copyStates.filter { s -> s.first.size == 3 && s.third < s.second &&
-                (s.first - RITestingFacility.E).any { it is Generator } &&  (s.first - RITestingFacility.E).any { it is MicroChip } }
-            .also { keepOne(it, states) }
-        /*println("pruning states:")
-        println(copyStates)
-        println("pruned to:")
-        println(states)*/
-    }
-
-    private fun keepOne(equivStates: List<Triple<Set<Any>, Int, Int>>, states: MutableSet<Triple<Set<Any>, Int, Int>>) {
-        equivStates.forEach { item ->
-            if (item != equivStates.first())
-                states.remove(item)
-        }
-    }
 
     override fun toString(): String = (1..4)
         .joinToString("\n") { i -> "${5 - i}: ${state.filter { e -> e.value == 5 - i }.map { it.key }}" }
