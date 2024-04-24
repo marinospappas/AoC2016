3b0ceb73dae5d1fcaf366226e41ec7f70a228a1e
marinos <marinosch.99981@gmail.com>
2024-04-24 11:40:23 +0200

    Day 15

--- src/main/kotlin/mpdev/springboot/aoc2016/solutions/day15/SpinningDisks.kt
+++ src/main/kotlin/mpdev/springboot/aoc2016/solutions/day15/SpinningDisks.kt
@@ -3,10 +3,7 @@ package mpdev.springboot.aoc2016.solutions.day15
 import kotlinx.serialization.Serializable
 import mpdev.springboot.aoc2016.input.InputDataReader
 import mpdev.springboot.aoc2016.solutions.PuzzleSolver
-import mpdev.springboot.aoc2016.utils.AocInClass
-import mpdev.springboot.aoc2016.utils.AocInField
-import mpdev.springboot.aoc2016.utils.AocInReplacePatterns
-import mpdev.springboot.aoc2016.utils.InputUtils
+import mpdev.springboot.aoc2016.utils.*
 import org.springframework.stereotype.Component
 
 @Component
@@ -15,22 +12,29 @@ class SpinningDisks(inputDataReader: InputDataReader): PuzzleSolver(inputDataRea
     var disks: List<Disk> = InputUtils(AoCInput::class.java).readAoCInput<AoCInput>(inputData)
         .map { Disk(it.id, it.positions, it.start) }
 
-    override fun initialize() {
+    private fun findCommonSlot(diskList: List<Disk>): Int {
+        for (t in 0 .. MAX_TIME)
+            if (diskList.all { disk -> disk.isSlotOpen(t + disk.id) })
+                return t
+        throw AocException("no solution found")
     }
 
-    override fun solvePart1(): Int {
-        return 0
-    }
+    override fun solvePart1() = findCommonSlot(disks)
 
-    override fun solvePart2(): Int {
-        return 0
-    }
+    override fun solvePart2() = findCommonSlot(
+        disks.toMutableList().also { list -> list.add(Disk(disks.last().id + 1, 11, 0)) }
+    )
 
     companion object {
+        const val MAX_TIME = 100_000_000
     }
 }
 
-data class Disk(val id: Int, val numPositions: Int, val startPosition: Int)
+data class Disk(val id: Int, val numPositions: Int, val startPosition: Int) {
+    // the disk is at pos.0 (slot open) at times: t = (numPositions - startPosition) + numPositions x N
+    //  where N is Integer
+    fun isSlotOpen(t: Int) = (t - (numPositions - startPosition)) % numPositions == 0
+}
 
 @Serializable
 @AocInClass(delimiters = [","])
--- src/test/kotlin/mpdev/springboot/aoc2016/day15/Day15Test.kt
+++ src/test/kotlin/mpdev/springboot/aoc2016/day15/Day15Test.kt
@@ -1,6 +1,7 @@
 package mpdev.springboot.aoc2016.day15
 
 import mpdev.springboot.aoc2016.input.InputDataReader
+import mpdev.springboot.aoc2016.solutions.day15.Disk
 import mpdev.springboot.aoc2016.solutions.day15.SpinningDisks
 import mpdev.springboot.aoc2016.utils.println
 import org.assertj.core.api.Assertions.assertThat
@@ -30,20 +31,51 @@ class Day15Test {
     @Order(2)
     fun `Reads Input`() {
         solver.disks.forEach { it.println() }
+        assertThat(solver.disks.size).isEqualTo(2)
+    }
+
+    @Test
+    @Order(3)
+    fun `Test timing of slot open`() {
+        val expected = listOf(
+            listOf(1, 6, 11, 16, 21),
+            listOf(1, 3, 5, 7, 9)
+        )
+        solver.disks.forEach { disk ->
+            val times = mutableListOf<Int>()
+            for (t in 0 .. 30)
+                if (disk.isSlotOpen(t))
+                    times.add(t)
+            println("disk: ${disk.id}, times for open slot: $times")
+            assertThat(times.subList(0, 5)).isEqualTo(expected[disk.id - 1])
+        }
+    }
+
+    @Test
+    @Order(5)
+    fun `Test timing of additional disk`() {
+        val expected = listOf(0, 11, 22, 33, 44, 55, 66, 77, 88, 99)
+        val disk = Disk(3, 11, 0)
+        val times = mutableListOf<Int>()
+        for (t in 0 .. 200)
+            if (disk.isSlotOpen(t))
+                times.add(t)
+        println("disk: ${disk.id}, times for open slot: $times")
+        assertThat(times.subList(0, 10)).isEqualTo(expected)
     }
 
     @Test
     @Order(4)
     fun `Solves Part 1`() {
         val result = solver.solvePart1().also { it.println() }
-        assertThat(result).isEqualTo(11)
+        assertThat(result).isEqualTo(5)
     }
 
     @Test
     @Order(6)
     fun `Solves Part 2`() {
-        println("NA")
-        assert(true)
+        val result = solver.solvePart2().also { it.println() }
+        assertThat(result).isEqualTo(85)
     }
 }
 
